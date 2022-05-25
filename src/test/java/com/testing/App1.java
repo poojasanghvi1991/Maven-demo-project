faa9d6b5fdc727a33bf1d978db8b4ff4c53cbdf2
Pooja S <poojarathod08@gmail.com>
2022-05-24 21:12:14 -0700

    m	new file:   src/test/java/com/testing/App1.java

---
+++ src/test/java/com/testing/App1.java
@@ -0,0 +1,14 @@
+package com.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Bye-Bye");
+    }
+}
---
+++ src/test/java/com/testing/test2.java
@@ -0,0 +1,14 @@
+package com.testing;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=8;
+		int c=a*b;
+		System.out.println("c=" +c); 
+
+	}
+
+}
